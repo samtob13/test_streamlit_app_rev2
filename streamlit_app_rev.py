8f86fdc642ff8ab77b7e01e89c577e04711184a3
samtob13 <115522100+samtob13@users.noreply.github.com>
2024-12-05 10:06:13 +0700

    Add files via upload

---
+++ streamlit_app_rev.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+
+# Display main content
+st.title("Welcome to uncertainty!!!")
+st.write("Use The sidebar to Move to Other Pages")
+st.write("Move to 'Running Script' Pages to Run the Calculation")
+st.write("Move to 'Summary' Pages to See the Results")
+
+# --- PAGE SETUP ---
+about_page = st.Page(
+    "views/Running_Script_rev.py",
+    title="Running Script",
+    icon=":material/account_circle:",
+)
+project_1_page = st.Page(
+    "views/Summary_rev.py",
+    title="Summary",
+    icon=":material/bar_chart:",
+)
+
+
+# --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
+# pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
+
+# --- NAVIGATION SETUP [WITH SECTIONS]---
+pg = st.navigation(
+    {
+        "Menu": [about_page, project_1_page],
+    }
+)
+
+
+# --- SHARED ON ALL PAGES ---
+# st.logo("assets/codingisfun_logo.png")
+st.sidebar.markdown("Reference from [Sven](https://youtube.com/@codingisfun)")
+
+
+# --- RUN NAVIGATION ---
+pg.run()
